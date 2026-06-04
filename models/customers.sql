c3879af13c3ba3a567f0635492a65d6ab94a5dee
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2026-06-04 20:51:40 +0100

    fin

--- models/customers.sql
+++ models/customers.sql
@@ -1,23 +1,14 @@
+{{ config(materialized = 'view') }}
+
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from {{ ref('raw_customers') }}
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from {{ ref('raw_orders') }}
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
